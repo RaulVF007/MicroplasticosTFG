3a1ccc1028d541a41f711d7180cc88f233752e1d
RaulVF007 <raulfv007@gmail.com>
2023-03-28 17:24:21 +0100

    Updated data augmentation script for horizontal flips in original images

--- microplasticos/data-augmentation.py
+++ microplasticos/data-augmentation.py
@@ -11,7 +11,9 @@ def data_augmentation():
             img = cv2.imread(os.path.join(directory, filename))
 
             img_flipped_vertical = cv2.flip(img, 0)
+            img_flipped_horizontal = cv2.flip(img, 1)
 
             cv2.imwrite(os.path.join(directory, name +"-ver-flip" + ext), img_flipped_vertical)
+            cv2.imwrite(os.path.join(directory, name + "-hor-flip" + ext), img_flipped_horizontal)
 
 data_augmentation()
