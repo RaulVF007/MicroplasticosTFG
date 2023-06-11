a4efd24a49046823f035299e921ac753c8270c8d
RaulVF007 <raulvf007@gmail.com>
2023-06-11 01:49:19 +0100

    Refactor json-to-txt script

--- microplasticos/json-to-txt-segmentation.py
+++ microplasticos/json-to-txt-segmentation.py
@@ -3,38 +3,44 @@ import json
 
 
 def json_to_txt_segmentation():
-    path_to_json = 'C:/Users/raulv/Documents/MicroplasticosTFG/json'
+    path_to_json = 'C:/Users/raulv/MicroplasticosTFG/json'
+    path_to_labels = 'C:/Users/raulv//MicroplasticosTFG/dataset/labels'
+
     for file in os.listdir(path_to_json):
-        with open(os.path.join(path_to_json, file), "r") as jsonFile:
-            json_data = json.load(jsonFile)
-        path_to_txt = os.path.join('C:/Users/raulv/Documents/MicroplasticosTFG/imagenes-microplasticos-etiquetar/labels', file)
-        with open(os.path.splitext(path_to_txt)[0] + ".txt", "w") as txt:
-            element = json_data['shapes'][0]['label']
-            objectsNumber = len(json_data['shapes'])
+        json_path = os.path.join(path_to_json, file)
+        with open(json_path, "r") as json_file:
+            json_data = json.load(json_file)
+        base_name = os.path.splitext(file)[0]
+        txt_path = os.path.join(path_to_labels, base_name + ".txt")
 
-            if element == 'Fragment':
-                numberClass = '0'
-            if element == 'Line':
-                numberClass = '1'
-            if element == 'Pellet':
-                numberClass = '2'
+        with open(txt_path, "w") as txt_file:
+            shapes = json_data['shapes']
+            for i, shape in enumerate(shapes):
+                element = shape['label']
+                if element == 'Fragment':
+                    number_class = '0'
+                elif element == 'Line':
+                    number_class = '1'
+                elif element == 'Pellet':
+                    number_class = '2'
+                else:
+                    number_class = '-1'
 
-            for object in range(objectsNumber):
-                if (object > 0):
-                    txt.write('\n')
-                txt.write(numberClass)
-                pointsNumber = len(json_data['shapes'][object]['points'])
-                content = ""
-                for point in range(pointsNumber):
-                    pointX = json_data['shapes'][object]['points'][point][0]
-                    pointY = json_data['shapes'][object]['points'][point][1]
+                if i > 0:
+                    txt_file.write('\n')
+                txt_file.write(number_class)
+                points = shape['points']
+
+                for j, point in enumerate(points):
+                    point_x = point[0]
+                    point_y = point[1]
                     width = json_data['imageWidth']
                     height = json_data['imageHeight']
-
-                    x = str(pointX/width)
-                    y = str(pointY/height)
+                    x = str(point_x / width)
+                    y = str(point_y / height)
                     content = " " + x + " " + y
-
-                    txt.write(content)
+                    txt_file.write(content)
+            json_file.close()
+            txt_file.close()
 
 json_to_txt_segmentation()
