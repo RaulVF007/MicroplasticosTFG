64f0ffb245ddbbe47acae9406ff9f1819e99bc45
RaulVF007 <raulvf007@gmail.com>
2023-07-02 02:20:19 +0100

    identifyMicroplastics improved for keeping same image format and colors from opening and saving

--- appMicroplasticos/main.py
+++ appMicroplasticos/main.py
@@ -1,21 +1,14 @@
 import os
-
-from PyQt5 import QtWidgets, QtGui, QtCore, uic
+from PyQt5 import QtWidgets, QtGui, uic
 import cv2
 import numpy as np
 import torch
-
-from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox
 from yolov5.utils.segment.general import process_mask
-
 from yolov5.utils.general import non_max_suppression, scale_boxes, Profile
-
 from yolov5.utils.plots import Annotator, colors
-
 from yolov5.models.common import DetectMultiBackend
 from yolov5.utils.torch_utils import select_device
-
 
 class GUI(QMainWindow):
     def __init__(self):
@@ -26,8 +19,11 @@ class GUI(QMainWindow):
         self.actionSave_results.setEnabled(False)
 
         self.fileName = ""
+        self.image = ""
+
         pixmap = QtGui.QPixmap(self.fileName)
         pixmap = pixmap.scaled(self.width(), self.height())
+
         self.label.setPixmap(pixmap)
         self.label.setMinimumSize(1, 1)
 
@@ -43,12 +39,23 @@ class GUI(QMainWindow):
 
     def openImage(self):
         options = QFileDialog.Options()
-        self.fileName, _ = QFileDialog.getOpenFileName(self, "Open image", "", "Image (*.png *.jpg *.jpeg)", options=options)
+        self.fileName, _ = QFileDialog.getOpenFileName(self, "Open image", "", "Image (*.png *.jpg *.jpeg)",
+                                                       options=options)
         if self.fileName:
             pixmap = QtGui.QPixmap(self.fileName)
             pixmap = pixmap.scaled(self.width(), self.height())
             self.label.setPixmap(pixmap)
+            self.image = pixmap
             self.actionIdentify_microplastics.setEnabled(True)
+
+    def resizeEvent(self, event):
+        try:
+            pixmap = QtGui.QPixmap(self.image)
+        except:
+            pixmap = QtGui.QPixmap("")
+        pixmap = pixmap.scaled(self.width(), self.height())
+        self.label.setPixmap(pixmap)
+        self.label.resize(self.width(), self.height())
 
     def identifyMicroplastics(self):
         device = select_device("cpu")
@@ -61,10 +68,10 @@ class GUI(QMainWindow):
 
         with dt[0]:
             im = torch.from_numpy(image).to(model.device)
-            im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
-            im /= 255  # 0 - 255 to 0.0 - 1.0
+            im = im.half() if model.fp16 else im.float()
+            im /= 255
             if len(im.shape) == 3:
-                im = im[None]  # expand for batch dim
+                im = im[None]
                 im = np.transpose(im, (0, 3, 1, 2))
 
         with dt[1]:
@@ -76,32 +83,38 @@ class GUI(QMainWindow):
         for i, det in enumerate(pred):
             annotator = Annotator(image, line_width=3, example=str(names))
             if len(det):
-                masks = process_mask(proto[i], det[:, 6:], det[:, :4], im.shape[2:], upsample=True)  # HWC
-                det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], image.shape).round()  # rescale boxes to im0 size
-                # Mask plotting
+                masks = process_mask(proto[i], det[:, 6:], det[:, :4], im.shape[2:], upsample=True)
+                det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], image.shape).round()
+
                 annotator.masks(
                     masks,
                     colors=[colors(x, True) for x in det[:, 5]],
-                    im_gpu=torch.as_tensor(image, dtype=torch.float16).to(device).permute(2, 0, 1).flip(0).contiguous())
-                # Write results
+                    im_gpu=torch.as_tensor(image, dtype=torch.float16).to(device).permute(2, 0, 1).flip(
+                        0).contiguous() / 255)
+
                 for j, (*xyxy, conf, cls) in enumerate(reversed(det[:, :6])):
                     c = int(cls)
                     label = f'{names[c]} {conf:.2f}'
                     annotator.box_label(xyxy, label, color=colors(c, True))
 
             image = annotator.result()
+            #Si muestro en este punto la imagen con un cv2.imshow("image",image) se ve perfecto
+   #         cv2.imshow("Image", image)
+   #         cv2.waitKey(0)
+   #         cv2.destroyAllWindows()
 
-        h, w, ch = image.shape
-        bytesPerLine = ch * w
-        convertToQtFormat = QtGui.QImage(image.data, w, h, bytesPerLine, QtGui.QImage.Format_RGB888)
-        p = convertToQtFormat.scaled(1024, 1280, Qt.KeepAspectRatio)
-        self.label.setPixmap(QtGui.QPixmap.fromImage(p))
+        height, width, channel = image.shape
+        bytes_per_line = channel * width
+        q_image = QtGui.QImage(image.data, width, height, bytes_per_line, QtGui.QImage.Format_BGR888)
+
+        pixmap = QtGui.QPixmap.fromImage(q_image)
+
+        self.image = q_image
+        self.label.setPixmap(pixmap)
+        self.label.setScaledContents(True)
         self.actionSave_results.setEnabled(True)
 
     def saveResults(self):
-        if not self.fileName:
-            return
-
         dir_name = QtWidgets.QFileDialog.getExistingDirectory(self, "Select location for saving result")
 
         file_name = os.path.basename(self.fileName)
@@ -109,11 +122,10 @@ class GUI(QMainWindow):
         new_file_name = f"{file_root}_identified{file_ext}"
         new_file_path = os.path.join(dir_name, new_file_name)
 
-        qimage = self.label.pixmap().toImage()
-        image = np.array(qimage.bits().asarray(qimage.width() * qimage.height() * 4)).reshape(
-            (qimage.height(), qimage.width(), 4))[:, :, :3]
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
+        qImage= self.label.pixmap().toImage()
+        resized_image = qImage.scaled(1280, 1024)
+        image = np.array(resized_image.bits().asarray(resized_image.width() * resized_image.height() * 4)).reshape(
+            (resized_image.height(), resized_image.width(), 4))[:, :, :3]
         cv2.imwrite(new_file_path, image)
 
         msg = QMessageBox()
